3964a5db9d26882646f1b0699e25dcd6fd64e3ac
shulk72 <54719564+shulk72@users.noreply.github.com>
2021-11-19 00:11:16 -0400

    Update lex.py
    change main function to a new file

--- lex.py
+++ lex.py
@@ -59,8 +59,3 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.Lexer.skip(1)
 
-if __name__ == '__main__':
-    data = ' [25/(3*40) + {300-20} -16.5]{(300-250)<(400-500)}20 & 30 | 50 # This is a comment'
-    Lexer = LexAnalyzer()
-    for tok in Lexer.tokenize(data):
-        print('type=%r, value=%r' % (tok.type, tok.value))
