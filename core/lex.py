dbe82693c44310a2325d5a9b023b1b3f1d0eb0d1
shulk72 <franklin.sifre@upr.edu>
2021-12-01 07:31:19 -0400

    yes

--- core/lex.py
+++ core/lex.py
@@ -1,4 +1,5 @@
 from sly import Lexer
+import numpy as np
 
 
 class LexAnalyzer(Lexer):
@@ -6,7 +7,9 @@ class LexAnalyzer(Lexer):
               DIVIDE, DOUBLEE, EQUAL, LPAREN, RPAREN, LBRACE,
               RBRACE, LBLOCK, RBLOCK, LTE, GTE, LT, GT,
               NOTE, AND, OR, COMMENT, IF, ELSE, ELSEIF, WHILE,
-              FOR, INTEGER, FLOAT, NEWLINE, PLOT, MATHFUNC, STATSFUNC
+              FOR, INTEGER, FLOAT, NEWLINE, PLOT, MATHFUNC, STATSFUNC, POW, NUMBER, STRING, TITLE, MOD,
+              ASSIGN, TICK, SEMI, COMMA, PIPE, CROCANTE, AT, DEGSYM, THEN, WITH, POLAR, CALC, EXIT,
+              SEMI, VARS, NEW, INT, FROM, TO, NEWLINE, AS, DIV, LIST,
               }
 
     # String containing ignored characters between tokens
@@ -15,6 +18,7 @@ class LexAnalyzer(Lexer):
     # Regular expression rules for tokens
     ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
     PLUS = r'\+'
+    POW = r'\^'
     MINUS = r'-'
     TIMES = r'\*'
     DIVIDE = r'/'
@@ -34,48 +38,63 @@ class LexAnalyzer(Lexer):
     AND = r'\&'
     OR = r'\|'
     COMMENT = r'\#.*'
+    NUMBER = r'([0-9]*[.])?[0-9]+(e\-?[1-9][0-9]*)?'
+    STRING = r'("[^\"]*")'
+    TITLE = r'\#.*'
+    MOD = r'%'
+    ASSIGN = r'='
+    NEWLINE = r'\n'
+    TICK = r"'"
+    SEMI = r';'
+    COMMA = r','
+    PIPE = r'\|'
+    CROCANTE = r'\$'
+    AT = r'@'
+    DEGSYM = r'<'
+    ID[''] = LIST
     ID['if'] = IF
-    ID['else']= ELSE
+    ID['else'] = ELSE
     ID['elseif'] = ELSEIF
     ID['while'] = WHILE
     ID['for'] = FOR
-    ID['sin']   = MATHFUNC
-    ID['cos']   = MATHFUNC
-    ID['tan']   = MATHFUNC
-    ID['asin']  = MATHFUNC
-    ID['acos']  = MATHFUNC
-    ID['atan']  = MATHFUNC
-    ID['sind']  = MATHFUNC
-    ID['cosd']  = MATHFUNC
-    ID['tand']  = MATHFUNC
+    ID['sin'] = MATHFUNC
+    ID['cos'] = MATHFUNC
+    ID['tan'] = MATHFUNC
+    ID['asin'] = MATHFUNC
+    ID['acos'] = MATHFUNC
+    ID['atan'] = MATHFUNC
+    ID['sind'] = MATHFUNC
+    ID['cosd'] = MATHFUNC
+    ID['tand'] = MATHFUNC
     ID['asind'] = MATHFUNC
     ID['acosd'] = MATHFUNC
     ID['atand'] = MATHFUNC
-    ID['rt']    = MATHFUNC
-    ID['ln']    = MATHFUNC
+    ID['rt'] = MATHFUNC
+    ID['ln'] = MATHFUNC
     ID['angle'] = MATHFUNC
-    ID['abs']   = MATHFUNC
+    ID['abs'] = MATHFUNC
     ID['plot'] = PLOT
-    ID['median'] = STATSFUNC
+
+    # Statistics
     ID['mean'] = STATSFUNC
-    ID['mode']= STATSFUNC
-    
+    ID['median'] = STATSFUNC
+    ID['mode'] = STATSFUNC
+
     @_(r'\d+')
     def INTEGER(self, t):
-      t.value = int(t.value)
-      return t
+        t.value = int(t.value)
+        return t
 
     @_(r'(\d*\.\d+)|(\d+\.\d*)')
     def FLOAT(self, t):
-      t.value = float(t.value)
-      return t
+        t.value = float(t.value)
+        return t
 
-# Define a rule so we can track line numbers
+    # Define a rule so we can track line numbers
     @_(r'\n+')
     def NEWLINE(self, t):
-      t.lexer.lineno += len(t.value)
+        t.lexer.lineno += len(t.value)
 
     def t_error(t):
         print("Illegal character '%s'" % t.value[0])
         t.Lexer.skip(1)
-
