f43f9fd6b29faef9a09ab5ce0856553861803548
jorge579 <jorge.perez33@upr.edu>
2021-12-01 07:58:41 -0400

    Added List and some code cleaning

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from lex import LexAnalyzer
 from parser import NParser, REPL
-# import matplotlib.pyplot as plt
+
 
 
 if __name__ == '__main__':
-    data = ' [25/(3*40) + {300-20} -16.5]{(300-250)<(400-500)}20 & 30 | 50 # This is a comment'
     lexer = LexAnalyzer()
     parser = NParser()
     REPL(lexer, parser)
-    # Lexer = LexAnalyzer()
-    # for tok in Lexer.tokenize(data):
-    #     print('type=%r, value=%r' % (tok.type, tok.value))
